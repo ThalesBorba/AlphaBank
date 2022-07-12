4a2bb63f4b2b3bfb804960ce6ac2768799b3087e
vandersonmauricio <vandermau678@gmail.com>
2022-07-12 14:45:11 -0300

    update card 322

--- src/main/java/com/foursys/fourcamp/alphabank/service/AccountAndTransactionService.java
+++ src/main/java/com/foursys/fourcamp/alphabank/service/AccountAndTransactionService.java
@@ -1,17 +1,5 @@
 package com.foursys.fourcamp.alphabank.service;
 
-import java.util.NoSuchElementException;
-
 public class AccountAndTransactionService {
-//    public String DeletRequest(oquevoupassarpradeletar){
-//
-//        try{
-//
-//        } catch (NoSuchElementException e){
-//            return "não tem uma requisição pendente com esse id";
-//        }
-//        return "Requisição de conta deletada com sucesso";
-//    }
-
 
 }
