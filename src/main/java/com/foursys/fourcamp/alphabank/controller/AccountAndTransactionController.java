c7810954adc9187902876e52bcf59fa37e99689a
Rafael de Oliveira <95747012+rafael-oliveira-hellz@users.noreply.github.com>
2022-07-15 09:57:12 -0300

    Feature/standing order detailed (#3)
    * First commit
    
    * add annotation notEmpty Name
    
    * change id
    
    * get detailed stading order
    
    Co-authored-by: Marcos Vinicius Santos Brito <marcos_aha2008@hotmail.com>

--- src/main/java/com/foursys/fourcamp/alphabank/controller/AccountAndTransactionController.java
+++ src/main/java/com/foursys/fourcamp/alphabank/controller/AccountAndTransactionController.java
@@ -1,24 +1,19 @@
 package com.foursys.fourcamp.alphabank.controller;
 
 import com.foursys.fourcamp.alphabank.exceptions.Handler;
+
+import com.foursys.fourcamp.alphabank.service.AccountAndTransactionService;
+
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import com.foursys.fourcamp.alphabank.dto.AccountRequestDTO;
-import com.foursys.fourcamp.alphabank.service.AccountAndTransactionService;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -26,49 +21,12 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class AccountAndTransactionController {
 
-
     private final AccountAndTransactionService accountAndTransactionService;
-
-    @Autowired
-    private ModelMapper modelMapper;
-
-    @Autowired
-    public AccountAndTransactionController(AccountAndTransactionService accountAndTransactionService) {
-        this.accountAndTransactionService = accountAndTransactionService;
-    }
-    
-    @PostMapping("/account-requests")
-    public ResponseEntity<Object> createAccountRequest(@RequestBody @Valid AccountRequestDTO accountRequestDTO, @PathVariable String xAbBankId, String
-            xAbPsuLastLogged, String xAbPsuIp, String xAbInteractionId, String xAbLang, String authorization, String
-                                                               ocpApimSubscriptionKey) {
-
-        return Handler.exceptionHandler(ResponseEntity.status(HttpStatus.CREATED).body(accountAndTransactionService.createAccountRequest(accountRequestDTO)));
-    }
-    
-    @Transactional
-    @DeleteMapping("/account-requests/{account-request-id}")
-    public ResponseEntity deleteAccountRequest(@PathVariable Long accountRequestId) {
-
-            accountAndTransactionService.DeleteAccountRequest(accountRequestId);
-
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
-    }
-}
 
     @GetMapping("/accounts/{account-id}/standing-orders/{standing-order-id}")
     public ResponseEntity<Object> returnStandingOrder(@PathVariable String accountId,@PathVariable String standingOrderId, String
             xAbBankId, String xAbPsuLastLogged, String xAbPsuIp, String xAbLang, String xAbInteractionId, String
                                                               authorization, String ocpApimSubscriptionKey) {
-        return Handler.exceptionHandler(ResponseEntity.status(HttpStatus.OK).body(accountAndTransactionService.
-                findByIdOrderDetailed(accountId, standingOrderId, xAbBankId, xAbPsuLastLogged, xAbPsuIp, xAbLang, xAbInteractionId, authorization, ocpApimSubscriptionKey)));
+        return Handler.exceptionHandler(ResponseEntity.status(HttpStatus.OK).body(accountAndTransactionService.findByIdOrderDetailed(accountId, standingOrderId, xAbBankId, xAbPsuLastLogged, xAbPsuIp, xAbLang, xAbInteractionId, authorization, ocpApimSubscriptionKey)));
     }
-
-    @GetMapping("/accounts/{account-id}/direct-debits/{direct-debit-id}")
-    public ResponseEntity<Object> returnDirectDebit(@PathVariable String accountId, @PathVariable String directDebitId, String
-            xAbBankId, String xAbPsuLastLogged, String xAbPsuIp, String xAbLang, String xAbInteractionId, String
-                                                            authorization, String ocpApimSubscriptionKey) {
-        return Handler.exceptionHandler(ResponseEntity.status(HttpStatus.OK).body(accountAndTransactionService.
-                findByIdDirectDebitsDetailed(accountId, directDebitId,  xAbBankId, xAbPsuLastLogged, xAbPsuIp, xAbLang, xAbInteractionId, authorization, ocpApimSubscriptionKey)));
-    }
-
 
--- src/main/java/com/foursys/fourcamp/alphabank/service/AccountAndTransactionService.java
+++ src/main/java/com/foursys/fourcamp/alphabank/service/AccountAndTransactionService.java
@@ -1,5 +1,8 @@
 package com.foursys.fourcamp.alphabank.service;
 
+import com.foursys.fourcamp.alphabank.dto.StandingOrderDetailedDTO;
+import com.foursys.fourcamp.alphabank.entities.StandingOrderDetailedInfo;
+
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
@@ -10,11 +13,10 @@ import com.foursys.fourcamp.alphabank.dto.AccountRequestDTO;
 import com.foursys.fourcamp.alphabank.entities.AccountRequest;
 import com.foursys.fourcamp.alphabank.repository.AccountRequestRepository;
 
-import com.foursys.fourcamp.alphabank.dto.StandingOrderDetailedDTO;
 import com.foursys.fourcamp.alphabank.entities.DirectDebitDetailedInfo;
-import com.foursys.fourcamp.alphabank.entities.StandingOrderDetailedInfo;
 import com.foursys.fourcamp.alphabank.repository.DirectDebitDetailedInfoRepository;
 import com.foursys.fourcamp.alphabank.repository.StandingOrderDetailedInfoRepository;
+
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -63,13 +65,3 @@ public class AccountAndTransactionService {
                 , detailed.getAccountId(), detailed.getAmount(), detailed.getCreditorAccount());
         return detailedDTO;
     }
-
-    public DirectDebitDetailedInfo findByIdDirectDebitsDetailed(String accountId, String directDebitId, String
-            xAbBankId, String xAbPsuLastLogged, String xAbPsuIp, String xAbLang, String xAbInteractionId, String
-                                                                        authorization, String ocpApimSubscriptionKey) {
-        DirectDebitDetailedInfo detailed = directDebitDetailedInfoRepository.findByIdAndAccountId(Long.valueOf(directDebitId), accountId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Débito direto não encontrado!"));
-        return detailed;
-    }
-
-
