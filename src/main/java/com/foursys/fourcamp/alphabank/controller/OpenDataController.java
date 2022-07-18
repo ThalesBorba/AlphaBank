562d2661c8fe5bbc51a92b0f5b9bd53a05d9c689
ThalesBorba <thales_borba@hotmail.com>
2022-07-18 09:20:49 -0300

    Delete lists

--- src/main/java/com/foursys/fourcamp/alphabank/controller/OpenDataController.java
+++ src/main/java/com/foursys/fourcamp/alphabank/controller/OpenDataController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -50,7 +51,7 @@ public class OpenDataController {
     //Tirei os parametros, irei ve como vou fazer
     @GetMapping(value = "/atms")
     public ResponseEntity<List<BankAtmsDTO>> returnBankAtms() {
-        return ResponseEntity.ok().body(openDataService.findAllAtms().stream().map(x -> mapper.map(x, BankAtmsDTO.class)).collect(Collectors.toList()));
+        return ResponseEntity.ok().body(openDataService.findAllAtms().stream().map(x -> mapper.map(x, BankAtmsDTO.class)).collect(Collectors.toCollection(ArrayList::new)));
     }
 
 }
--- src/main/java/com/foursys/fourcamp/alphabank/entities/DirectDebitsDetailedInfo.java
+++
@@ -1,9 +0,0 @@
-package com.foursys.fourcamp.alphabank.entities;
-
-import java.io.Serializable;
-import java.util.List;
-
-public class DirectDebitsDetailedInfo implements Serializable {
-
-    List<DirectDebitDetailedInfo> directDebitDetailedInfos;
-}
