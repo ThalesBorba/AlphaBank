cf7cba5b44b6b2658c9d5059cce1c90e16d1f976
ThalesBorba <thales_borba@hotmail.com>
2022-07-21 17:27:30 -0300

    Era email, Diogo!

--- src/main/java/com/foursys/fourcamp/alphabank/config/MockAdmin.java
+++ src/main/java/com/foursys/fourcamp/alphabank/config/MockAdmin.java
@@ -18,7 +18,7 @@ public class MockAdmin implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        User adm = new User(null, "adm", "000.000.000-00", "$2a$12$WZWz7pqNiVWX5VVJKC4u0uTYmSRnnfoxHRHubOeX316/IhroXn/EW");
+        User adm = new User(null, "adm@gmail.com", "000.000.000-00", "$2a$12$WZWz7pqNiVWX5VVJKC4u0uTYmSRnnfoxHRHubOeX316/IhroXn/EW");
         userRepository.save(adm);
 
         Role role = new Role("USER");
