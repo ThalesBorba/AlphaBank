19bd98b0a2c10592096888c97bf92c3796344edd
ThalesBorba <thales_borba@hotmail.com>
2022-07-21 18:42:09 -0300

    Agora eu tô sentindo

--- src/main/java/com/foursys/fourcamp/alphabank/config/security/SecurityConfiguration.java
+++ src/main/java/com/foursys/fourcamp/alphabank/config/security/SecurityConfiguration.java
@@ -78,7 +78,7 @@ public class SecurityConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowedOrigins("http://localhost:5173")
+        registry.addMapping("/**").allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT");}
 
     @Bean
