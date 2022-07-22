bd0d97c39674087d46e73e13fb24b4bceb39aba6
ThalesBorba <thales_borba@hotmail.com>
2022-07-21 22:32:08 -0300

    Felicidade?

--- src/main/java/com/foursys/fourcamp/alphabank/AlphabankApplication.java
+++ src/main/java/com/foursys/fourcamp/alphabank/AlphabankApplication.java
@@ -2,15 +2,10 @@ package com.foursys.fourcamp.alphabank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class AlphabankApplication implements WebMvcConfigurer {
+public class AlphabankApplication {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("GET", "POST", "DELETE", "OPTIONS");}
 	public static void main(String[] args) { SpringApplication.run(AlphabankApplication.class, args);}
 
 }
